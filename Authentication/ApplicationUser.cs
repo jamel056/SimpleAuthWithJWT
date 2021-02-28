e31abac5ea3127b78873f258c9fb11b52dc42893
JAMEEL ALEDLBI <JAMEL_056@hotmail.com>
2021-02-28 21:28:37 +0300

    Add project files.

---
+++ Authentication/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthWithJWT.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
---
+++ Authentication/LogInModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthWithJWT.Authentication
+{
+    public class LogInModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
